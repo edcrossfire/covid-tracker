6326d0b1a15d6fcef0d41fc608caa141ea328f02
edcrossfire <ed@crossfire.network>
2022-07-21 00:46:18 -0400

    Vue setup and data connect

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="h-screen w-screen bg-indigo-50">
+      <div class="bg-gradient-to-br from-indigo-400 to-indigo-700 text-center p-4 text-4xl lg:text-5xl text-indigo-50 mb-10 font-bold max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg mx-auto shadow-xl rounded-b-xl">
+          <Header />
+      </div>
+      <main v-if="!loading">
+        <DataTitle :text="title" :dataDate="dataDate" />
+      </main>
+      <main class="flex flex-col items-center space-y-3" v-else>
+        <div class="text-2xl">
+          Fetching Data
+        </div>
+        <img :src="loadingImage" alt="loading" class="w-12 m-auto" />
+      </main>
+    </div>
+    
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "@/components/Header.vue"
+import DataTitle from "@/components/DataTitle.vue"
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
+    Header,
+    DataTitle,
+  },
+  data() {
+    return {
+      loading: true,
+      title: 'Global',
+      dataDate: '',
+      status: {},
+      countries: [],
+      loadingImage: require('./assets/loading.gif')
+    }
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch('https://api.covid19api.com/summary')
+      const data = await res.json()
+      return data
+    }
+  },
+  async created() {
+    const data = await this.fetchCovidData()
+    this.dataDate = data.dataDate
+    this.stats = data.Global
+    this.countries = data.countries
+    this.loading = false
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header>
+    <div>
+      <i class="fa-solid fa-virus-covid"></i> COVID-19 Tracker
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
